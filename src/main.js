2aa35c1282a224dc53dfa26939f268a1c92f25a1
Ernesto R. C. Pereda <ercp42@gmail.com>
2019-07-23 06:56:46 -0300

    Install bootstrap

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
 import VueAwesomeSwiper from "vue-awesome-swiper";
 
 import App from "./App.vue";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import "swiper/dist/css/swiper.css";
+
 import "./assets/scss/index.scss";
 
+Vue.use(BootstrapVue);
 Vue.use(VueAwesomeSwiper /* { default global options } */);
 
 Vue.config.productionTip = false;
