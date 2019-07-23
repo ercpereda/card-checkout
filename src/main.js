82fc854d9c115801a9d2db1ebec47f16e8404239
Ernesto R. C. Pereda <ercp42@gmail.com>
2019-07-22 23:21:53 -0300

    Setup vue-awesome-swiper

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+
 import App from "./App.vue";
 
+import "swiper/dist/css/swiper.css";
 import "./assets/scss/index.scss";
+
+Vue.use(VueAwesomeSwiper /* { default global options } */);
 
 Vue.config.productionTip = false;
 
